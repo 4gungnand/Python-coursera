bb774c816c22531bf97594cf0bd5244317c0684e
I Gusti Agung Premananda <94607130+Premaaa@users.noreply.github.com>
2023-08-23 22:01:49 +0700

    Add files via upload

---
+++ celcius.py
@@ -0,0 +1,23 @@
+# def to_celcius(x):
+#     return (x-32)*5/9
+
+# for x in range(0,101,10):
+#     print("{:>3} F | {:>6.2f} C".format(x, to_celcius(x)))
+
+Weather = "Rainfall"
+
+print(Weather.endswith("fall"))
+
+car_make = "hippopotamus"
+
+print(car_make[3:6])
+
+colors = ["red", "white", "blue"]
+colors.insert(2, "yellow")
+print(colors)
+
+teacher_names = {"Math" : "Aniyah Cook"}
+
+genre = "Transcendental"
+
+print(genre[:-8])
